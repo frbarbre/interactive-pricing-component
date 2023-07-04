58159c0b5a32393218129958e0e8018e5e8044be
frbarbre <fr.barbre@gmail.com>
2023-07-04 02:45:37 +0200

    turned animations back on

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,7 +3,9 @@ import { motion as m } from "framer-motion";
 export default function Header() {
   return (
     <m.header
-
+      initial={{ opacity: 0, y: -100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{duration: 0.35, delay: 0.5}}
       className="w-full max-w-[400px] mx-auto text-center bg-bg-circles bg-no-repeat bg-center bg-contain py-6 my-8"
     >
       <h1 className="text-[24px] font-extrabold text-cta-bg">
